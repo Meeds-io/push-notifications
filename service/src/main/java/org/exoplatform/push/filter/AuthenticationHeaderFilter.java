fe8b84bd89ddca3ea3518595c4218782842e6c9c
Thomas Delhoménie <tdelhomenie@exoplatform.com>
2018-02-05 14:42:40 +0100

    Add custom header to allow iOS versions older than iOS 11 to read authentication cookies values

---
+++ service/src/main/java/org/exoplatform/push/filter/AuthenticationHeaderFilter.java
@@ -0,0 +1,65 @@
+package org.exoplatform.push.filter;
+
+import org.exoplatform.web.filter.Filter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * Filter setting authentication headers in a custom header. This is required by
+ * iOS version older than iOS 11 since they cannot read cookies headers directly.
+ */
+public class AuthenticationHeaderFilter implements Filter {
+
+  public static final String   HEADER_AUTHORIZATION = "X-Authorization";
+
+  public static final String[] COOKIES_TO_PROPAGATE = new String[] { "JSESSIONID", "JSESSIONIDSSO", "rememberme" };
+
+  @Override
+  public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException,
+                                                                                                  ServletException {
+    HttpServletRequest req = (HttpServletRequest) request;
+    HttpServletResponse res = (HttpServletResponse) response;
+
+    if (req.getRemoteUser() != null) {
+      Map<String, String> authorizationHeaders = new HashMap<>();
+      Cookie[] cookies = req.getCookies();
+
+      for (String cookieName : COOKIES_TO_PROPAGATE) {
+        String cookieValue = getCookieValue(cookies, cookieName);
+        if (cookieValue != null) {
+          authorizationHeaders.put(cookieName, cookieValue);
+        }
+      }
+
+      if (!authorizationHeaders.isEmpty()) {
+        String authorizationHeaderValue = authorizationHeaders.keySet()
+                                                              .stream()
+                                                              .map(key -> key + "=" + authorizationHeaders.get(key))
+                                                              .collect(Collectors.joining(";"));
+        res.addHeader(HEADER_AUTHORIZATION, authorizationHeaderValue);
+      }
+    }
+
+    filterChain.doFilter(req, res);
+    return;
+  }
+
+  protected String getCookieValue(Cookie[] cookies, Object key) {
+    for (int i = 0; i < cookies.length; i++) {
+      if (key.equals(cookies[i].getName())) {
+        return cookies[i].getValue();
+      }
+    }
+    return null;
+  }
+}
---
+++ service/src/test/java/org/exoplatform/push/filter/AuthenticationHeaderFilterTest.java
@@ -0,0 +1,120 @@
+package org.exoplatform.push.filter;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+
+public class AuthenticationHeaderFilterTest {
+
+  @Test
+  public void shouldReturnConcatenatedHeaderWithUserAuthenticated() throws IOException, ServletException {
+    // Given
+    HttpServletRequest request = mock(HttpServletRequest.class);
+    HttpServletResponse response = mock(HttpServletResponse.class);
+    FilterChain filterChain = mock(FilterChain.class);
+
+    when(request.getRemoteUser()).thenReturn("john");
+    List<Cookie> cookies = new ArrayList<>();
+    for (String cookieName : AuthenticationHeaderFilter.COOKIES_TO_PROPAGATE) {
+      cookies.add(new Cookie(cookieName, "value" + cookieName));
+    }
+    when(request.getCookies()).thenReturn(cookies.toArray(new Cookie[0]));
+
+    AuthenticationHeaderFilter filter = new AuthenticationHeaderFilter();
+
+    ArgumentCaptor<String> headerNameCaptor = ArgumentCaptor.forClass(String.class);
+    ArgumentCaptor<String> headerValueCaptor = ArgumentCaptor.forClass(String.class);
+
+    // When
+    filter.doFilter(request, response, filterChain);
+
+    // Then
+    verify(response).addHeader(headerNameCaptor.capture(), headerValueCaptor.capture());
+
+    String name = headerNameCaptor.getValue();
+    String value = headerValueCaptor.getValue();
+    assertNotNull(name);
+    assertEquals(AuthenticationHeaderFilter.HEADER_AUTHORIZATION, name);
+    assertNotNull(value);
+    List<String> authenticationHeaderTokens = Arrays.asList(value.split(";"));
+    assertEquals(3, authenticationHeaderTokens.size());
+    assertTrue(authenticationHeaderTokens.contains(AuthenticationHeaderFilter.COOKIES_TO_PROPAGATE[0] + "=value"
+        + AuthenticationHeaderFilter.COOKIES_TO_PROPAGATE[0]));
+    assertTrue(authenticationHeaderTokens.contains(AuthenticationHeaderFilter.COOKIES_TO_PROPAGATE[1] + "=value"
+        + AuthenticationHeaderFilter.COOKIES_TO_PROPAGATE[1]));
+    assertTrue(authenticationHeaderTokens.contains(AuthenticationHeaderFilter.COOKIES_TO_PROPAGATE[2] + "=value"
+        + AuthenticationHeaderFilter.COOKIES_TO_PROPAGATE[2]));
+  }
+
+  @Test
+  public void shouldReturnConcatenatedHeaderWithNoUserAuthenticated() throws IOException, ServletException {
+    // Given
+    HttpServletRequest request = mock(HttpServletRequest.class);
+    HttpServletResponse response = mock(HttpServletResponse.class);
+    FilterChain filterChain = mock(FilterChain.class);
+
+    when(request.getRemoteUser()).thenReturn(null);
+    List<Cookie> cookies = new ArrayList<>();
+    for (String cookieName : AuthenticationHeaderFilter.COOKIES_TO_PROPAGATE) {
+      cookies.add(new Cookie(cookieName, "value" + cookieName));
+    }
+    when(request.getCookies()).thenReturn(cookies.toArray(new Cookie[0]));
+
+    AuthenticationHeaderFilter filter = new AuthenticationHeaderFilter();
+
+    // When
+    filter.doFilter(request, response, filterChain);
+
+    // Then
+    verify(response, never()).addHeader(any(), any());
+  }
+
+  @Test
+  public void shouldReturnConcatenatedHeaderWithUserAuthenticatedAndMissingCookies() throws IOException, ServletException {
+    // Given
+    HttpServletRequest request = mock(HttpServletRequest.class);
+    HttpServletResponse response = mock(HttpServletResponse.class);
+    FilterChain filterChain = mock(FilterChain.class);
+
+    when(request.getRemoteUser()).thenReturn("john");
+    List<Cookie> cookies = new ArrayList<>();
+    cookies.add(new Cookie("JSESSIONID", "valueJSESSIONID"));
+    cookies.add(new Cookie("JSESSIONIDSSO", "valueJSESSIONIDSSO"));
+    cookies.add(new Cookie("UNRELATED_COOKIE", "value"));
+    when(request.getCookies()).thenReturn(cookies.toArray(new Cookie[0]));
+
+    AuthenticationHeaderFilter filter = new AuthenticationHeaderFilter();
+
+    ArgumentCaptor<String> headerNameCaptor = ArgumentCaptor.forClass(String.class);
+    ArgumentCaptor<String> headerValueCaptor = ArgumentCaptor.forClass(String.class);
+
+    // When
+    filter.doFilter(request, response, filterChain);
+
+    // Then
+    verify(response).addHeader(headerNameCaptor.capture(), headerValueCaptor.capture());
+
+    String name = headerNameCaptor.getValue();
+    String value = headerValueCaptor.getValue();
+    assertNotNull(name);
+    assertEquals(AuthenticationHeaderFilter.HEADER_AUTHORIZATION, name);
+    assertNotNull(value);
+    List<String> authenticationHeaderTokens = Arrays.asList(value.split(";"));
+    assertEquals(2, authenticationHeaderTokens.size());
+    assertTrue(authenticationHeaderTokens.contains("JSESSIONID=valueJSESSIONID"));
+    assertTrue(authenticationHeaderTokens.contains("JSESSIONIDSSO=valueJSESSIONIDSSO"));
+  }
+}
