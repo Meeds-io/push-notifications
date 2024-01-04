cd0a754b8b0f2c034e64f7e3b755033808d786c1
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2024-01-04 07:55:20 +0100

    fix: Fix JPA Entity allocationSize - Meeds-io/MIPs#57 Prior to this change, the Liquibase 'allocationSize' for a sequence is by default put to 1 unlike the JPA default allocation size which is put to 50. This change will ensure to put the liquibase default allocationSize in JPA entity.

--- service/src/main/java/org/exoplatform/push/domain/Device.java
+++ service/src/main/java/org/exoplatform/push/domain/Device.java
@@ -42,7 +42,7 @@ import java.util.Date;
 public class Device {
 
   @Id
-  @SequenceGenerator(name="SEQ_MSG_DEVICES_ID", sequenceName="SEQ_MSG_DEVICES_ID")
+  @SequenceGenerator(name="SEQ_MSG_DEVICES_ID", sequenceName="SEQ_MSG_DEVICES_ID", allocationSize = 1)
   @GeneratedValue(strategy=GenerationType.AUTO, generator="SEQ_MSG_DEVICES_ID")
   @Column(name = "ID")
   private long id;
